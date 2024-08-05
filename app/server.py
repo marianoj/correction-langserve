df26d0f7029fc9c539bb37082f66d820d2564d9d
Mariano J. <marianojponce@gmail.com>
2024-08-05 13:49:51 +0800

    feat(package): add robocorp

--- app/server.py
+++ app/server.py
@@ -4,9 +4,9 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langserve import add_routes
 from pirate_speak.chain import chain as pirate_speak_chain
+from robocorp_action_server import agent_executor as robocorp_action_server_chain
 
 app = FastAPI()
-
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -18,6 +18,7 @@ runnable = ChatPromptTemplate.from_template("Tell me a joke about {topic}") | Ch
 # Edit this to add the chain you want to add
 add_routes(app, runnable)
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
+add_routes(app, robocorp_action_server_chain, path="/robocorp-action-server")
 
 if __name__ == "__main__":
     import uvicorn
