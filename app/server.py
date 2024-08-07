f84de55198e4c4a5534e5bfea21f526bb4cf9ff0
Mariano J. <marianojponce@gmail.com>
2024-08-07 18:46:02 +0800

    feat(server): add a simple example

--- app/server.py
+++ app/server.py
@@ -1,26 +1,47 @@
+#!/usr/bin/env python
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langserve import add_routes
 from pirate_speak.chain import chain as pirate_speak_chain
-from robocorp_action_server import agent_executor as robocorp_action_server_chain
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
-# Define your runnable
-runnable = ChatPromptTemplate.from_template("Tell me a joke about {topic}") | ChatOpenAI()
+add_routes(
+    app,
+    ChatOpenAI(model="gpt-3.5-turbo-0125"),
+    path="/openai",
+)
 
-# Edit this to add the chain you want to add
-add_routes(app, runnable)
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
-add_routes(app, robocorp_action_server_chain, path="/robocorp-action-server")
+model = ChatOpenAI(model="gpt-4o-mini")
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+add_routes(
+    app,
+    prompt | model,
+    path="/joke",
+)
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
